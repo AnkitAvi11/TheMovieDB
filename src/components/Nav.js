318e7034acf7824d6f70fcf7114b95dffaaeeaf0
Ankit Kumar <kumar.ankit38@gmail.com>
2020-11-11 20:42:05 +0530

    Home page and navigation bar added

---
+++ src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+<div className="ui container" style={{padding:"15px 0px"}}>
+        <div className="ui secondary  menu">
+        <Link to="/" className="item">
+          <b>The Movie DB</b>
+        </Link>
+        <NavLink to="/movies" className="item">
+          Movies
+        </NavLink>
+        <NavLink to="/tvshows" className="item">
+          TV Shows
+        </NavLink>
+        <NavLink to="/people" className="item">
+          People
+        </NavLink>
+        <div className="right menu">
+          <div className="item">
+            <div className="ui icon input">
+              <input type="text" placeholder="Search movies ..." />
+              <i className="search link icon"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+export default Nav;
---
+++ src/containers/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <p>Home page</p>
+            </div>
+        )
+    }
+
+}
+
+export default Home;
